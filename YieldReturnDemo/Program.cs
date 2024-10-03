4843bb0b1c95e9c0a9c9cc39949db562f7d5eee2
Knyihár Gábor <gabor.knyihar@aut.bme.hu>
2024-10-03 13:06:44 +0200

    Yield return demo

---
+++ YieldReturnDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YieldReturnDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var j in GetNumbers())
+                Console.WriteLine(j);
+        }
+
+        static IEnumerable<int> GetNumbers()
+        {
+            for (int i = 0; i < 5; i++)
+                yield return i;
+        }
+    }
+}
